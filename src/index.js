edb937a628249ebb2d12102901c1801956156238
ipyatihatko <elijah.pyatikhatko@gmail.com>
2024-07-09 19:48:32 +0300

    webpack, folder structure, configs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./style.scss";
+import "./style.scss"
+import template from './templates/main.hbs'
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (!document.getElementById('root')) {
+    const fragment = document.createRange().createContextualFragment(template());
+    document.body.appendChild(fragment);
+  }
+
+  // Check for environment variable and log it (or perform other actions based on the environment)
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Running in development mode');
+  } else {
+    console.log('Running in production mode');
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +8,43 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/templates/main.hbs',
+      filename: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    watchFiles: {
+      paths: ['src/**/*.*', 'assets/**/*.*'],
+      options: {
+        usePolling: true,
+      },
+    },
+  },
+  // stats: 'errors-only'
 };
